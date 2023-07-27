6c78ed5bd054d4bdb2a5140d46c30ac4af2794fe
ForwardingAgent <nikolaspdk@gmail.com>
2023-07-27 18:38:21 +0300

    commit

--- store/products/models.py
+++ store/products/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 # Create your models here.
+# models - таблицы для БД
 
 
-class ProductCategory(models.Model):
-    name = models.CharField(max_length=128)
-    description = models.TextField(null=True, blank=True)
+class ProductCategory(models.Model):  # 3.3 3:10 models.Model этот класс позволяет класс Python перенести на SQL язык 
+    name = models.CharField(max_length=128)  # 3.3 класс CharField строка с определенным набором символов
+    description = models.TextField(null=True, blank=True)  # 3.3 TextField строка с текстом, null-может быть пустым
 
     def __str__(self):
         return self.name
