f0445c21429278a3e62ceb98af38615ea475f69d
ForwardingAgent <nikolaspdk@gmail.com>
2023-08-19 09:21:59 +0300

    some descriptions

--- store/users/admin.py
+++ store/users/admin.py
@@ -12,7 +12,3 @@ from products.admin import BasketAdmin
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username',)  # отвечает за то как я хочу видеть все продукты на гл.странице, сейчас строка из products/models.py def __str__(self)...
     inlines = (BasketAdmin,)  # добавялет в админке user'а внизу корзину Basket со всеми товарами
-    #fields = ('name', 'description', 'price', 'quantity', 'image', 'category')  # порядок можно менять и поменяются в admin'ке | кортеж в кортеже тогда два значения на одной линии
-    #readonly_fields = ('description')  # делает поле неизменяемым
-    #search_fields = ('name',)  # добавляет поле поиска
-    #ordering = ('name',)  # отображение списка в алфавитном порядке | ('-name') в обратном алфав порядке
